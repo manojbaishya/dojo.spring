79f6518f30e5ec76b02eb4e6f48b8dbd720c3c52
Manoj Baishya <28330014+manojbaishya@users.noreply.github.com>
2025-11-17 19:27:50 +0530

    Add isNullOrEmpty() method to StringUtils
    Add isNullOrEmpty() method to StringUtils

--- src/main/java/org/dojo/spring/shared/utilities/StringUtils.java
+++ src/main/java/org/dojo/spring/shared/utilities/StringUtils.java
@@ -2,4 +2,6 @@ package org.dojo.spring.shared.utilities;
 
 public class StringUtils {
     public static boolean isNotNullOrEmpty(String string) { return string != null && !string.isBlank(); }
+
+    public static boolean isNullOrEmpty(String checkString) { return checkString == null || checkString.isBlank(); }
 }
